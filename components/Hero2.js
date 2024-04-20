dca8bcf1e7c3b37b6f1664cde0ce63b828ef6ec5
gnafhan <nafhanghifari@gmail.com>
2024-04-20 16:25:28 +0700

    fix name

--- components/Hero2.js
+++ components/Hero2.js
@@ -6,19 +6,19 @@ const Hero2 = () => {
     <>
       <Container className='py-0'>
         <div className='flex items-center w-full '>
-          <div className='max-w-screen-xl mb-8 w-full'>
+          <div className='w-full max-w-screen-xl mb-8'>
             <div className='lg:h-[60vh] h-[30vh] w-full relative '>
               <Image
                 src={'/img/banner.png'}
                 alt='Banner'
-                className='w-full object-cover rounded-lg'
+                className='object-cover w-full rounded-lg'
                 fill={true}
                 draggable={false}
               />
             </div>
-            <div className='py-10 flex justify-center flex-col items-center gap-2'>
-              <h1 className='text-7xl font-bold text-primary'>
-                Smart<span className='text-secondary'>Plate</span>
+            <div className='flex flex-col items-center justify-center gap-2 py-10'>
+              <h1 className='font-bold text-7xl text-primary'>
+                Nutri<span className='text-secondary'>Plate</span>
               </h1>
               <h4>Portioning Precision, Health in Every Division</h4>
             </div>
--- components/Manfaat.js
+++ components/Manfaat.js
@@ -4,28 +4,28 @@ import Container from './container'
 const Manfaat = () => {
   return (
     <>
-      <Container className=' rounded-xl lg:bg-gradient-custom '>
-        <div className='flex flex-col md:flex-row justify-start gap-x-10 items-center bg-gradient-custom lg:bg-none p-5 rounded-lg    '>
-          <div className='flex lg:w-5/12 w-full lg:justify-end '>
-            <div className='w-full flex justify-end'>
+      <Container className=' rounded-xl lg:bg-gradient-custom'>
+        <div className='flex flex-col items-center justify-start p-5 rounded-lg md:flex-row gap-x-10 bg-gradient-custom lg:bg-none '>
+          <div className='flex w-full lg:w-5/12 lg:justify-end '>
+            <div className='flex justify-end w-full'>
               <div className='relative w-full lg:w-10/12 h-96'>
                 <Image
                   src={'/img/piring.png'}
                   fill
-                  className='flex  object-contain'
+                  className='flex object-contain'
                   alt='Piring'
                 />
               </div>
             </div>
           </div>
-          <div className='flex lg:w-6/12 flex-col gap-3'>
-            <div className='bg-white dark:bg-black rounded-full py-3 px-1'>
-              <h1 className='font-bold text-4xl text-center text-primary'>
-                Manfaat <span className='text-secondary'>SmartPlate</span>
+          <div className='flex flex-col gap-3 lg:w-6/12'>
+            <div className='px-1 py-3 bg-white rounded-full dark:bg-black'>
+              <h1 className='text-4xl font-bold text-center text-primary'>
+                Manfaat <span className='text-secondary'>NutriPlate</span>
               </h1>
             </div>
-            <p className='text-justify leading-relaxed text-white font-poppins'>
-              SmartPlate merupakan piring pemorsian yang bertujuan untuk
+            <p className='leading-relaxed text-justify text-white font-poppins'>
+              Nutriplate merupakan piring pemorsian yang bertujuan untuk
               mengatur porsi makan individu disesuaikan dengan kondisi
               kesehatannya. Piring ini akan memberikan edukasi dan sebagai
               tindakan preventif kepada masyarakat mengenai pola makan yang

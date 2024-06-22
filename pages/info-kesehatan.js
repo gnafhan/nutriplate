8a3ac285bf3f04e94e38404ac8fae917e893b559
gnafhan <nafhanghifari@gmail.com>
2024-06-22 19:37:37 +0700

    push info kesehatan

--- pages/info-kesehatan.js
+++ pages/info-kesehatan.js
@@ -38,34 +38,36 @@ const Home = () => {
           className='absolute bottom-0 left-0'
         />
         <Container className='z-10'>
-          <h1 className='mt-6 mb-4 text-3xl font-semibold text-primary'>
+          <h1 className='mt-6 mb-4 text-3xl font-semibold text-black'>
             Berita dan Jurnal Kesehatan
           </h1>
-          <div className='grid grid-cols-3 gap-5'>
-            <div className="px-6 py-16 text-white rounded-lg bg-[url('/img/bg-makanan.png')] bg-cover">
-              <div className=''>
-                <h1 className='text-lg font-medium '>
-                  Makanan super, keserbagunaannya untuk kesehatan kulit
-                </h1>
-                <p className='text-sm mt-14'>1 JAM YANG LALU</p>
+
+          <div className='grid grid-cols-1 gap-5 md:grid-cols-3'>
+            <a href='/blog/manajemen-diabetes' className='relative block h-64 group sm:h-80 lg:h-96'>
+              <span className='absolute inset-0 border-2 border-black border-dashed rounded-md'></span>
+
+              <div className='relative flex items-end h-full transition-transform transform bg-white border-2 border-black rounded-md border-bottom-4 border-right-4 group-hover:-translate-x-2 group-hover:-translate-y-2'>
+                <div className='p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8'>
+                  <img src='/img/DiabetesBlog.jpg' className='hidden object-cover border border-black rounded-md md:block size-1 sm:size-6' />
+
+                  <h2 className='mt-4 text-xl font-medium sm:text-2xl'>
+                    Manajemen Diet Diabetes Melitus, Apa Itu?
+                  </h2>
+                </div>
+
+                <div className='absolute p-4 transition-opacity opacity-0 group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8'>
+                  <h3 className='mt-4 text-xl font-medium sm:text-2xl'>
+                    Manajemen Diet Diabetes Melitus, Apa Itu?
+                  </h3>
+
+                  <p className='mt-4 text-sm sm:text-base'>
+                  Pada artikel kali ini kita akan membahas mengenai manajemen diet pada diabetes melitus, yuk simak penjelasan berikut ini! 
+                  </p>
+
+                  <p className='mt-8 font-bold'>Baca Selengkapnya!</p>
+                </div>
               </div>
-            </div>
-            <div className="px-6 py-16 text-white rounded-lg bg-[url('/img/bg-makanan.png')] bg-cover">
-              <div className=''>
-                <h1 className='text-lg font-medium '>
-                  Makanan super, keserbagunaannya untuk kesehatan kulit
-                </h1>
-                <p className='text-sm mt-14'>1 JAM YANG LALU</p>
-              </div>
-            </div>
-            <div className="px-6 py-16 text-white rounded-lg bg-[url('/img/bg-makanan.png')] bg-cover">
-              <div className=''>
-                <h1 className='text-lg font-medium '>
-                  Makanan super, keserbagunaannya untuk kesehatan kulit
-                </h1>
-                <p className='text-sm mt-14'>1 JAM YANG LALU</p>
-              </div>
-            </div>
+            </a>
           </div>
         </Container>
       </section>

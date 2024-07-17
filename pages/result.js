fc89a31acd7d086254735e634e0eedcd8c5f0293
gnafhan <nafhanghifari@gmail.com>
2024-07-17 17:10:45 +0700

    fix

--- pages/result.js
+++ pages/result.js
@@ -1,163 +1,168 @@
-import Image from 'next/image'
-import React, { useContext, useEffect, useRef } from 'react'
-import { DataContext } from '../context/DataContext'
+// import Image from 'next/image'
+// import React, { useContext, useEffect, useRef } from 'react'
+// import { DataContext } from '../context/DataContext'
 
-const predictionData = [
-  {
-    mid: '/m/047vlmn',
-    name: 'Outerwear',
-    score: 0.59962904,
-    boundingPoly: {
-      normalizedVertices: [
-        {
-          x: 0.0099487305,
-          y: 0.40429688
-        },
-        {
-          x: 0.96875,
-          y: 0.40429688
-        },
-        {
-          x: 0.96875,
-          y: 0.9921875
-        },
-        {
-          x: 0.0099487305,
-          y: 0.9921875
-        }
-      ]
-    }
-  },
-  {
-    mid: '/m/09j2d',
-    name: 'Clothing',
-    score: 0.58714676,
-    boundingPoly: {
-      normalizedVertices: [
-        {
-          x: 0.0099487305,
-          y: 0.40429688
-        },
-        {
-          x: 0.96875,
-          y: 0.40429688
-        },
-        {
-          x: 0.96875,
-          y: 0.9921875
-        },
-        {
-          x: 0.0099487305,
-          y: 0.9921875
-        }
-      ]
-    }
-  },
-  {
-    mid: '/m/01g317',
-    name: 'Person',
-    score: 0.5839288,
-    boundingPoly: {
-      normalizedVertices: [
-        {
-          x: 0.02734375,
-          y: 0.025268555
-        },
-        {
-          x: 0.96875,
-          y: 0.025268555
-        },
-        {
-          x: 0.96875,
-          y: 1
-        },
-        {
-          x: 0.02734375,
-          y: 1
-        }
-      ]
-    }
-  }
-]
+// const predictionData = [
+//   {
+//     mid: '/m/047vlmn',
+//     name: 'Outerwear',
+//     score: 0.59962904,
+//     boundingPoly: {
+//       normalizedVertices: [
+//         {
+//           x: 0.0099487305,
+//           y: 0.40429688
+//         },
+//         {
+//           x: 0.96875,
+//           y: 0.40429688
+//         },
+//         {
+//           x: 0.96875,
+//           y: 0.9921875
+//         },
+//         {
+//           x: 0.0099487305,
+//           y: 0.9921875
+//         }
+//       ]
+//     }
+//   },
+//   {
+//     mid: '/m/09j2d',
+//     name: 'Clothing',
+//     score: 0.58714676,
+//     boundingPoly: {
+//       normalizedVertices: [
+//         {
+//           x: 0.0099487305,
+//           y: 0.40429688
+//         },
+//         {
+//           x: 0.96875,
+//           y: 0.40429688
+//         },
+//         {
+//           x: 0.96875,
+//           y: 0.9921875
+//         },
+//         {
+//           x: 0.0099487305,
+//           y: 0.9921875
+//         }
+//       ]
+//     }
+//   },
+//   {
+//     mid: '/m/01g317',
+//     name: 'Person',
+//     score: 0.5839288,
+//     boundingPoly: {
+//       normalizedVertices: [
+//         {
+//           x: 0.02734375,
+//           y: 0.025268555
+//         },
+//         {
+//           x: 0.96875,
+//           y: 0.025268555
+//         },
+//         {
+//           x: 0.96875,
+//           y: 1
+//         },
+//         {
+//           x: 0.02734375,
+//           y: 1
+//         }
+//       ]
+//     }
+//   }
+// ]
 
-const result = () => {
-  const { data, setData, screenshot, setScreenshot, size } =
-    useContext(DataContext)
-  const canvasRef = useRef(null)
-  const imageRef = useRef(null)
-s
-  const saveImageToLocalStorage = () => {
-    localStorage.setItem('capturedImage', screenshot)
-  }
+// const result = () => {
+//   const { data, setData, screenshot, setScreenshot, size } =
+//     useContext(DataContext)
+//   const canvasRef = useRef(null)
+//   const imageRef = useRef(null)
+// s
+//   const saveImageToLocalStorage = () => {
+//     localStorage.setItem('capturedImage', screenshot)
+//   }
 
-  useEffect(() => {
-    if (!screenshot) {
-      setScreenshot(localStorage.getItem('capturedImage'))
-    } else {
-      saveImageToLocalStorage()
-    }
+//   useEffect(() => {
+//     if (!screenshot) {
+//       setScreenshot(localStorage.getItem('capturedImage'))
+//     } else {
+//       saveImageToLocalStorage()
+//     }
 
-    // setData(predictionData)
-  }, [])
+//     // setData(predictionData)
+//   }, [])
 
-  const handleCanvas = () => {
-    const canvas = canvasRef.current
-    const ctx = canvas.getContext('2d')
-    const image = imageRef.current
-    const color = ['#5BBCFF', '#FFFAB7', '#FFD1E3', '#7EA1FF']
-    const textColor = ['#000000', '#000000', '#000000', '#000000']
+//   const handleCanvas = () => {
+//     const canvas = canvasRef.current
+//     const ctx = canvas.getContext('2d')
+//     const image = imageRef.current
+//     const color = ['#5BBCFF', '#FFFAB7', '#FFD1E3', '#7EA1FF']
+//     const textColor = ['#000000', '#000000', '#000000', '#000000']
 
-    canvas.width = image.width
-    canvas.height = image.height
-    data.map((prediction, index) => {
-      const x = prediction.boundingPoly.normalizedVertices[0].x * image.width
-      const y = prediction.boundingPoly.normalizedVertices[0].y * image.height
-      const width =
-        (prediction.boundingPoly.normalizedVertices[1].x -
-          prediction.boundingPoly.normalizedVertices[0].x) *
-        image.width
-      const height =
-        (prediction.boundingPoly.normalizedVertices[2].y -
-          prediction.boundingPoly.normalizedVertices[0].y) *
-        image.height
+//     canvas.width = image.width
+//     canvas.height = image.height
+//     data.map((prediction, index) => {
+//       const x = prediction.boundingPoly.normalizedVertices[0].x * image.width
+//       const y = prediction.boundingPoly.normalizedVertices[0].y * image.height
+//       const width =
+//         (prediction.boundingPoly.normalizedVertices[1].x -
+//           prediction.boundingPoly.normalizedVertices[0].x) *
+//         image.width
+//       const height =
+//         (prediction.boundingPoly.normalizedVertices[2].y -
+//           prediction.boundingPoly.normalizedVertices[0].y) *
+//         image.height
 
-      if (index > color.length - 1) {
-        index = index % color.length
-      }
+//       if (index > color.length - 1) {
+//         index = index % color.length
+//       }
 
-      ctx.strokeStyle = color[index]
-      ctx.lineWidth = 4
-      ctx.strokeRect(x, y, width, height)
-      ctx.fillStyle = color[index]
-      const textWidth = ctx.measureText(prediction.name).width
-      const textHeight = parseInt('16px sans-serif', 10)
-      ctx.fillRect(x, y, textWidth + 4, textHeight + 4)
+//       ctx.strokeStyle = color[index]
+//       ctx.lineWidth = 4
+//       ctx.strokeRect(x, y, width, height)
+//       ctx.fillStyle = color[index]
+//       const textWidth = ctx.measureText(prediction.name).width
+//       const textHeight = parseInt('16px sans-serif', 10)
+//       ctx.fillRect(x, y, textWidth + 4, textHeight + 4)
 
-      ctx.fillStyle = textColor[index]
-      ctx.fillText(prediction.name, x, y + 10)
-    })
-  }
+//       ctx.fillStyle = textColor[index]
+//       ctx.fillText(prediction.name, x, y + 10)
+//     })
+//   }
 
-  const imageSrc = `data:image/jpeg;base64,${screenshot}`
-  return (
-    <>
-      <div className='container relative flex justify-center max-w-screen-sm p-0 mx-auto bg-cyan-400 xl:px-0 h-[50vh]'>
-        <Image
-          ref={imageRef}
-          className='absolute object-cover'
-          src={imageSrc}
-          fill
-        />
-        <canvas ref={canvasRef} className='absolute z-10 ' />
-      </div>
-      <button
-        onClick={handleCanvas}
-        className='absolute p-4 text-white bg-black rounded-full opacity-50 bottom-10'
-      >
-        Show Canvas
-      </button>
-    </>
-  )
-}
+//   const imageSrc = `data:image/jpeg;base64,${screenshot}`
+//   return (
+//     <>
+//       <div className='container relative flex justify-center max-w-screen-sm p-0 mx-auto bg-cyan-400 xl:px-0 h-[50vh]'>
+//         <Image
+//           ref={imageRef}
+//           className='absolute object-cover'
+//           src={imageSrc}
+//           fill
+//         />
+//         <canvas ref={canvasRef} className='absolute z-10 ' />
+//       </div>
+//       <button
+//         onClick={handleCanvas}
+//         className='absolute p-4 text-white bg-black rounded-full opacity-50 bottom-10'
+//       >
+//         Show Canvas
+//       </button>
+//     </>
+//   )
+// }
 
-export default result
+// export default result
+
+
+export default function result(params) {
+  return (<></>)
+};

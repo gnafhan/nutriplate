4949de1b2a080ca7093c68dc2f6e97ebe338350c
user <user@users-MacBook-Air-2.local>
2024-12-02 01:07:34 +0700

    add

--- pages/premium.js
+++ pages/premium.js
@@ -1,69 +1,77 @@
-'use client'
-import Head from 'next/head'
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/navbar'
-import Image from 'next/image'
-import { useCookies } from 'react-cookie'
-import { collection, getDocs } from 'firebase/firestore'
-import db from '../lib/firebase'
-import { useRouter } from 'next/router'
+"use client";
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
+import Image from "next/image";
+import { useCookies } from "react-cookie";
+import { collection, getDocs } from "firebase/firestore";
+import db from "../lib/firebase";
+import { useRouter } from "next/router";
 
 const Premium = () => {
-    const [cookies, setCookie] = useCookies()
-    const [token, setToken] = useState('')
-    const [error, setError] = useState('')
-    
-    const handleSubmit = async ()=>{
-        try {
-            const tokenCollection = collection(db, 'tokens');
-            const tokenSnapshot = await getDocs(tokenCollection);
-            const tokens = tokenSnapshot.docs.map(doc => doc.data().token);
-            if (tokens.includes(token)) {
-                setCookie('token', token, { path: '/' })
-                if( !window.location.search.slice(6,999).split('%')[0]){
-                  window.location.href = '/home' 
-                } else{
-                  window.location.href = '/home' //+ window.location.search.slice(6,999).split('%')[0]
-                }
-            } else {
-              setError('Token tidak valid')
-            }
-        } catch (error) {
-            setError('Token tidak valid')
+  const [cookies, setCookie] = useCookies();
+  const [token, setToken] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const tokenCollection = collection(db, "tokens");
+      const tokenSnapshot = await getDocs(tokenCollection);
+      const tokens = tokenSnapshot.docs.map((doc) => doc.data().token);
+      if (tokens.includes(token)) {
+        setCookie("token", token, { path: "/" });
+        if (!window.location.search.slice(6, 999).split("%")[0]) {
+          window.location.href = "/home";
+        } else {
+          window.location.href = "/home"; //+ window.location.search.slice(6,999).split('%')[0]
         }
-        
+      } else {
+        setError("Token tidak valid");
+      }
+    } catch (error) {
+      setError("Token tidak valid");
     }
+  };
   return (
     <>
       <Head>
         <title>Nutriplate</title>
-        <meta name='description' content='Nutriplate' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Nutriplate" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
-      <div className='flex w-full justify-center items-center min-h-[80vh] px-12'>
-        <div className='border-[#D8D5D5] border-solid border-2 rounded-lg flex py-10 aspect-1 px-3 w-full  lg:w-1/4 justify-center items-center'>
-            <div className='flex flex-col items-center justify-between w-full gap-4'>
-                <Image src={'/img/logo.svg'} width={100} height={100} />
-                <div className='flex justify-center w-full'>
-                    <p className='text-xl font-semibold text-primary'>Masukkan kode/token</p>
-                </div>
-                <div className="w-full">
-                    <input onChange={(e)=> setToken(e.target.value)} type="text" id="last_name" class=" border border-primary text-gray- text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray- dark:border-gray- dark:placeholder-gray- dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123456AB" required />
-                    {error && <p className='text-red-500'>{error}</p>}
-                </div>
-                <button
-                    type='submit'
-                    className='px-5 py-3 font-semibold text-white rounded-md w-fit bg-primary'
-                    onClick={handleSubmit}
-                  >
-                    Submit
-                  </button>
+      <Navbar />
+      <div className="flex w-full justify-center items-center min-h-[80vh] px-12">
+        <div className="border-[#D8D5D5] border-solid border-2 rounded-lg flex py-10 aspect-1 px-3 w-full  lg:w-1/4 justify-center items-center">
+          <div className="flex flex-col items-center justify-between w-full gap-4">
+            <Image src={"/img/logo.svg"} width={100} height={100} />
+            <div className="flex justify-center w-full">
+              <p className="text-xl font-semibold text-primary">
+                Masukkan kode/token
+              </p>
             </div>
+            <div className="w-full">
+              <input
+                onChange={(e) => setToken(e.target.value)}
+                type="text"
+                id="last_name"
+                class=" border border-primary text-gray- text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray- dark:border-gray- dark:placeholder-gray- dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="123456AB"
+                required
+              />
+              {error && <p className="text-red-500">{error}</p>}
+            </div>
+            <button
+              type="submit"
+              className="px-5 py-3 font-semibold text-white rounded-md w-fit bg-primary"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Premium
+export default Premium;
--- pages/resep-makanan/index.js
+++ pages/resep-makanan/index.js
@@ -1,66 +1,70 @@
-import Head from 'next/head'
-import Navbar from '../../components/navbar'
+import Head from "next/head";
+import Navbar from "../../components/navbar";
 
-import Footer from '../../components/footer'
-import Image from 'next/image'
-import { CardResep } from '../../components/CardResep'
-import { tokenMiddleware } from '../../util/tokenMiddleware'
-import useTokenMiddleware from '../../hooks/useTokenMiddleware'
+import Footer from "../../components/footer";
+import Image from "next/image";
+import { CardResep } from "../../components/CardResep";
+import { tokenMiddleware } from "../../util/tokenMiddleware";
+import useTokenMiddleware from "../../hooks/useTokenMiddleware";
 
 const Home = () => {
-  useTokenMiddleware()
-    const data = [
-        {
-        nama: "Resep Normal",
-        link: '/resep-makanan/normal',
-        deskripsi: "resep makanan normal"
-        },
-        {
-        nama: "Resep Diabetes",
-        link: '/resep-makanan/diabetes',
-        deskripsi: "resep makanan diabetes"
-        },
-        {
-        nama: "Resep Atlet",
-        link: '/resep-makanan/atlet',
-        deskripsi: "resep makanan atlet"
-        },
-        {
-        nama: "Resep Obesitas",
-        link: '/resep-makanan/obesitas',
-        deskripsi: "resep makanan obesitas"
-        },
-]
+  useTokenMiddleware();
+  const data = [
+    {
+      nama: "Resep Makanan",
+      link: "/resep-makanan/normal",
+      deskripsi: "resep makanan normal",
+    },
+    // {
+    // nama: "Resep Diabetes",
+    // link: '/resep-makanan/diabetes',
+    // deskripsi: "resep makanan diabetes"
+    // },
+    // {
+    // nama: "Resep Atlet",
+    // link: '/resep-makanan/atlet',
+    // deskripsi: "resep makanan atlet"
+    // },
+    // {
+    // nama: "Resep Obesitas",
+    // link: '/resep-makanan/obesitas',
+    // deskripsi: "resep makanan obesitas"
+    // },
+  ];
   return (
     <>
       <Head>
         <title>Nutriplate</title>
-        <meta name='description' content='Nutriplate' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Nutriplate" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
-      <section className='relative flex w-full min-h-screen mx-5'>
+      <section className="relative flex w-full min-h-screen mx-5">
         <Image
-          src={'/img/shape_pinggir.svg'}
+          src={"/img/shape_pinggir.svg"}
           width={427}
           height={364}
-          className='absolute bottom-0 right-0 z-[-1] hidden lg:block'
+          className="absolute bottom-0 right-0 z-[-1] hidden lg:block"
         />
         <Image
-          src={'/img/shape_pinggir.svg'}
+          src={"/img/shape_pinggir.svg"}
           width={427}
           height={364}
-          className='absolute bottom-0 left-0 scale-x-[-1] z-[-1] hidden lg:block'
+          className="absolute bottom-0 left-0 scale-x-[-1] z-[-1] hidden lg:block"
         />
-        <div className='flex justify-center w-full h-full'>
-          <div className='flex justify-center w-full max-w-screen-lg gap-y-8 '>
-            <div className='grid items-center justify-center w-full h-full grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-10 place-items-center'>
-            {data.map((item)=>{
-                return(
-               <CardResep nama={item.nama} link={item.link} deskripsi={item.deskripsi} />     
-                )
-            })}
+        <div className="flex justify-center w-full h-full">
+          <div className="flex justify-center w-full max-w-screen-lg gap-y-8 ">
+            <div className="grid items-center justify-center w-full h-full grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-10 place-items-center">
+              {data.map((item) => {
+                return (
+                  <CardResep
+                    nama={item.nama}
+                    link={item.link}
+                    deskripsi={item.deskripsi}
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
@@ -68,11 +72,11 @@ const Home = () => {
       <Footer />
       {/* <PopupWidget /> */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export const getServerSideProps = async ({ req, res }) => {
-  return tokenMiddleware(req, res)
-}
+  return tokenMiddleware(req, res);
+};
